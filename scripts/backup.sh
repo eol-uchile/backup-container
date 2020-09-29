536ad8f2cad145a976947f1bed4b61d2208c97a6
Felipe Espinoza <felipe.espinoza.r@uchile.cl>
2020-09-29 19:01:18 +0000

    Update nfs volume, removing platform name

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -16,7 +16,7 @@ sh /root/scripts/execute.sh $folder
 # Copy to nfs
 if [ -d /volume/nfs ]
 then
-tar -cf - $folder | openssl aes-256-cbc -md sha256 -salt -out /volume/nfs/$PLATFORM_NAME/daily/$datenow.tar.enc -pass pass:"$BACKUP_PASSWORD" || true
+tar -cf - $folder | openssl aes-256-cbc -md sha256 -salt -out /volume/nfs/daily/$datenow.tar.enc -pass pass:"$BACKUP_PASSWORD" || true
 fi
 
 # Copy to gdrive
