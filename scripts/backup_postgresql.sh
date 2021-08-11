4cb6aa2caa26f85a6330fb5ec55f8fcb97c06d66
Dario Andhael <spam.dario@gmail.com>
2021-08-11 17:17:26 -0400

    Add back a different script for postgresql

---
+++ scripts/backup_postgresql.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Minio takes to long to create a backup. This should only do daily
+# routines to avoid cluster cpu throtling.
+
+echo "Starting backup"
+set -eu
+
+# Configure
+sh /root/scripts/configure.sh
+
+# Daily
+BASE=/tmp/backup
+datenow=$(date +%Y%m%d)
+folder=$BASE/$datenow
+mkdir -p $folder
+
+outfolder=daily/$datenow
+
+echo "mkdir done"
+
+# Run minio backup
+echo "Doing Minio Backups"
+sh /root/scripts/services/postgresql.sh $folder $outfolder
+
+rm -fr $BASE
