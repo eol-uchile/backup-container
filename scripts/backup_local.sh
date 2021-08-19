079d6d5a1c7f4afbbc93e9546ed24d133fc8bf51
Dario Andhael <spam.dario@gmail.com>
2021-08-19 18:17:13 -0400

    Copy backups to local filesystem script

---
+++ scripts/backup_local.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "Starting backup copy to local"
+set -eu
+
+# Configure
+sh /root/scripts/configure.sh
+
+# Daily or hourly
+option=$1
+out=$2
+
+# Date should be in format 2014-06-28
+init=$3
+days=$4
+
+if [ $option == 'daily' ]
+then
+  for i in $(eval echo {1..$days}); do 
+    # custom format using +
+    datenow=$(date +%Y%m%d -d "$init +$i days")
+    remote_folder=$PLATFORM_NAME/$option/$datenow
+    folder=$out/remote_folder
+    echo "Downloading $remote_folder from Drive"
+    rclone copy gdrive:/$remote_folder $folder
+  done
+else
+  for i in {1..24}; do 
+    remote_folder=$PLATFORM_NAME/$option/$i
+    folder=$out/remote_folder
+    echo "Downloading $remote_folder from Drive"
+    rclone copy gdrive:/$remote_folder $folder
+  done
+fi
