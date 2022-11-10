c5b9c819fdae6b3854359ff8a4b79907802c6a13
matiasua <matiasncp7@gmail.com>
2022-11-10 13:10:00 -0300

    Removed --vv in minio.sh

--- scripts/services/minio.sh
+++ scripts/services/minio.sh
@@ -11,7 +11,7 @@ MINIO_PID=$!
 
 for i in $(echo $PLATFORM_S3_BUCKETS | sed "s/,/ /g")
 do
-  rclone copy -vv --s3-disable-checksum --ignore-errors --ignore-checksum  --transfers 16 source:$i destination:$i || true
+  rclone copy --s3-disable-checksum --ignore-errors --ignore-checksum  --transfers 16 source:$i destination:$i || true
 done
 
 kill $MINIO_PID
