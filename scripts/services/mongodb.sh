06086e883d7dfc1ed3c20be4021ad7b56ac3e7be
fsologureng <fsologur@uchile.cl>
2025-08-26 13:46:25 -0400

    feat(mongodb.sh): implement mongo backup wo username

--- scripts/services/mongodb.sh
+++ scripts/services/mongodb.sh
@@ -5,7 +5,12 @@ folder=$1
 remote_folder=$2
 
 # Dump mongodb
-mongodump --host "$PLATFORM_MONGODB_HOST" --port $PLATFORM_MONGODB_PORT --username "$PLATFORM_MONGODB_USER" --password "$PLATFORM_MONGODB_PASSWORD" --authenticationDatabase "$PLATFORM_MONGODB_DB" --archive --db "$PLATFORM_MONGODB_DB" | gzip > $folder/mongodb_openedx.gz
+if [ "$PLATFORM_MONGODB_USER" = "" ]
+then
+  mongodump --host "$PLATFORM_MONGODB_HOST" --port $PLATFORM_MONGODB_PORT --archive --db "$PLATFORM_MONGODB_DB" | gzip > $folder/mongodb_openedx.gz
+else
+  mongodump --host "$PLATFORM_MONGODB_HOST" --port $PLATFORM_MONGODB_PORT --username "$PLATFORM_MONGODB_USER" --password "$PLATFORM_MONGODB_PASSWORD" --authenticationDatabase "$PLATFORM_MONGODB_DB" --archive --db "$PLATFORM_MONGODB_DB" | gzip > $folder/mongodb_openedx.gz
+fi
 
 openssl aes-256-cbc -md sha256 -salt -out $folder/mongodb_openedx.gz.enc -in $folder/mongodb_openedx.gz -pass pass:"$BACKUP_PASSWORD"
 rm $folder/mongodb_openedx.gz
