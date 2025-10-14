9014043a22a01f6b7e0f9cd9fb35c400db1bba92
fsologureng <fsologur@uchile.cl>
2025-10-14 13:58:40 -0300

    chore(new/services): simplify scripts & remove error handling
    (cherry picked & adapted from commit db2bd40ecf95a3434227f85eb381510e397335d6)

--- scripts/services/moodle.sh
+++ scripts/services/moodle.sh
@@ -5,21 +5,14 @@ folder=$1
 remote_folder=$2
 
 # Dump Moodle data
-tar -zcf $folder/moodle.tar.gz $PLATFORM_MOODLE_DATA || rm -fv $folder/moodle.tar.gz
+tar -zcf $folder/moodle.tar.gz $PLATFORM_MOODLE_DATA
 
-if [ -f $folder/moodle.tar.gz ]
-then
-  # Cipher & delete source
-  sh /root/scripts/cipher.sh $folder/moodle.tar.gz $folder/moodle.tar.gz.enc
-  if [ -f $folder/moodle.tar.gz.enc ]
-  then
-    echo "Uploading to NAS"
-    rclone copy $folder/moodle.tar.gz.enc nas:/share/eol_backup/$PLATFORM_NAME/$remote_folder
+# Cipher & delete source
+sh /root/scripts/cipher.sh $folder/moodle.tar.gz $folder/moodle.tar.gz.enc
 
-    rm -rf $folder/moodle.tar.gz.enc
-  else
-    echo "Skipping uploading to NAS"
-  fi
-else
-  echo "Skipping ciphering"
-fi
+echo "Uploading to NAS"
+rclone copy $folder/moodle.tar.gz.enc nas:/share/eol_backup/$PLATFORM_NAME/$remote_folder
+echo "Uploaded to NAS"
+
+# Remove compressed&encrypted file
+rm -rf $folder/moodle.tar.gz.enc
--- scripts/services/postgresql.sh
+++ scripts/services/postgresql.sh
@@ -5,28 +5,20 @@ folder=$1
 remote_folder=$2
 
 # Dump
-pg_dump --clean --host="$PLATFORM_POSTGRESQL_HOST" --port=$PLATFORM_POSTGRESQL_PORT --username="$PLATFORM_POSTGRESQL_USER" $PLATFORM_POSTGRESQL_DATABASES | gzip > $folder/postgresql.gz || rm -fv $folder/postgresql.gz
-if [ -f $folder/postgresql.gz ] && [ -s $folder/postgresql.gz ]
-then
-  # Cipher & delete source
-  sh /root/scripts/cipher.sh $folder/postgresql.gz $folder/postgresql.gz.enc
-  if [ -f $folder/postgresql.gz.enc ]
-  then
-    echo "Uploading to NAS"
-    rclone copy $folder/postgresql.gz.enc nas:/share/eol_backup/$PLATFORM_NAME/$remote_folder
-    echo "Uploaded to NAS"
+pg_dump --clean --host="$PLATFORM_POSTGRESQL_HOST" --port=$PLATFORM_POSTGRESQL_PORT --username="$PLATFORM_POSTGRESQL_USER" $PLATFORM_POSTGRESQL_DATABASES | gzip > $folder/postgresql.gz
 
-    # Clean or move to mount for recovery
-    if [ $3 = 'keep' ]
-    then
-      mkdir -p $HOST_MOUNT/$PLATFORM_NAME
-      mv $folder/postgresql.gz.enc $HOST_MOUNT/$PLATFORM_NAME
-    else
-      rm $folder/postgresql.gz.enc
-    fi
-  else
-    echo "Skipping uploading to NAS"
-  fi
+# Cipher & delete source
+sh /root/scripts/cipher.sh $folder/postgresql.gz $folder/postgresql.gz.enc
+
+echo "Uploading to NAS"
+rclone copy $folder/postgresql.gz.enc nas:/share/eol_backup/$PLATFORM_NAME/$remote_folder
+echo "Uploaded to NAS"
+
+# Clean or move to mount for recovery
+if [ $3 = 'keep' ]
+then
+  mkdir -p $HOST_MOUNT/$PLATFORM_NAME
+  mv $folder/postgresql.gz.enc $HOST_MOUNT/$PLATFORM_NAME
 else
-  echo "Skipping ciphering"
+  rm $folder/postgresql.gz.enc
 fi
